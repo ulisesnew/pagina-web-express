73f498af170c3d1c43e77cf33fa054cb4188fb48
ulisesnew <un9074174@gmail.com>
2024-07-20 17:56:08 -0600

    Add files via upload

--- validar.php
+++ validar.php
@@ -4,12 +4,20 @@ include 'conexion.php';
 
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
-$usuario = $_POST['usuario'];
-$correo_electronico =$_POST['correo'];
-$password = $_POST['contraseña'];
+$usuario = $_POST['username'];
+$correo =$_POST['correo'];
+$contrasena = $_POST['contrasena'];
 
 
-$sql = "INSERT INTO usuarios (Nombre, Apellidos, Usuarios, Correo, Contraseña, ) VALUES ('$nombre', '$apellido', '$usuario', '$correo','$contraseña');";
+$sql = "INSERT INTO usuarios (Nombre, Apellido, Correo, Usuario, Contrasena) VALUES 
+('$nombre', '$apellido', '$correo', '$usuario','$contrasena');";
 
 $resultado = mysqli_query($db,$sql);
+
+if($resultado){
+    echo "Usuario agregado correctamente";
+} else{
+    echo "Algo ha fallado". mysqli_error($db) ;
+}
+
 ?>
